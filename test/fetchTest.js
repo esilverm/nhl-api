fd16a03442d678cbc1c97befc21ed8555fa31460
esilverm <evan7silverman@gmail.com>
2018-02-03 13:43:08 -0500

    add travis file

--- test/fetchTest.js
+++ test/fetchTest.js
@@ -1,12 +1,6 @@
 const nhl = require('./src')
 const fetch = require('./src/utils/fetch.js')
-/*
+
 fetch("https://statsapi.web.nhl.com/api/v1/teams?season=19691970").then((res) => {
   console.log(res)
-})
-*/
-nhl.Teams.get().then((res) => {
-  console.log(res)
-}).catch((error) => {
-  console.log(error)
 })
